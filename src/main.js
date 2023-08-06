1b5a04b4ee6cb4b401b1274237d1f2f890e8bfd2
Alvintooong <aeptechnologies@gmail.com>
2023-08-06 08:39:02 +0800

    Added firebase config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from './plugins/firebase'
+import { getAnalytics } from "firebase/analytics";
+
+getAnalytics(firebase)
 
 const app = createApp(App)
 
