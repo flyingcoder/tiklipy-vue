0c34d304b0e77b493303d60fd8a3afd877650ba0
Francis Canayon <canayonfrancis@gmail.com>
2023-08-07 15:41:23 +0800

    Add footer in app page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-  import Header from './views/Header.vue'
-
+  import Header from './views/Header.vue';
+  import Footer from './views/Footer.vue'
 </script>
 <template>
   <Header />
   <router-view />
+  <Footer />
 </template>
