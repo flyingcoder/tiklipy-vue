c32de71b27c925490bb680baa39e1906d6868c68
Alvintooong <aeptechnologies@gmail.com>
2023-08-09 04:43:34 +0800

    Fix sign-in page

--- src/views/sign/In.vue
+++ src/views/sign/In.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import { ref } from "vue"
+    import { ref, defineProps, onMounted } from "vue"
+    import { Modal } from "flowbite"
     import { useRouter } from "vue-router"
     import { getAuth, 
         signInWithEmailAndPassword,
@@ -8,7 +9,27 @@
         signInWithPopup, 
     } from "firebase/auth";
 
-    const showModal = ref(true);
+    const $targetEl = document.getElementById('modalEl');
+
+    // options with default values
+    const options = {
+        placement: 'bottom-right',
+        backdrop: 'dynamic',
+        backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+        closable: true,
+        onHide: () => {
+            console.log('modal is hidden');
+        },
+        onShow: () => {
+            console.log('modal is shown');
+        },
+        onToggle: () => {
+            console.log('modal has been toggled');
+        }
+    };
+
+    const modal = new Modal($targetEl, options)
+    modal.show();
 
     const router = useRouter();
     const email = ref("");
@@ -51,18 +72,22 @@
 </script>
 
 <template>
-    <div class="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center">
-        <div class="relative w-full max-w-md max-h-full">
+    <div id="modalEl" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <div class="w-full px-8 py-6">
+                <!-- Modal header -->
+                <div class="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-600">
                     <img class="w-32 m-auto" src="/tiklipy-logo-indigo.png" alt="tiklipy logo indigo color">
                     <h2 class="text-2xl text-center">Sign in to Start!</h2>
                     <p class="text-lg text-center">
                         Start generating fantastic lessons!
                     </p>
                 </div>
-                <div class="items-center justify-center w-full px-6">
-                    <p class="text-sm text-center">Login with Emal & Password</p>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <div class="items-center justify-center w-full">
+                        <p class="text-sm text-center">Login with Emal & Password</p>
                     <div class="my-2">
                     <label for="email" class="block mb-2 text-sm font-medium">
                         Email</label>
@@ -70,34 +95,32 @@
                         placeholder="Enter a valid email">
                     </div>
                     <div class="mb-2">
-                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Password</label>
-                    <input v-model="password" type="password" id="password" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="Enter a password">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            Password</label>
+                        <input v-model="password" type="password" id="password" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Enter a password">
                     </div>
                     <button  @click="signIn" :disabled="!email || !password" :class="(!email || !password) || isLoading ? 'cursor-not-allowed bg-gray-500 hover:bg-gray-500' : 'bg-blue-500 hover:bg-blue-700'" class="w-full py-2 mt-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
-                    {{ isLoading ? "Loading..." : "Login" }}
+                        {{ isLoading ? "Loading..." : "Login" }}
                     </button>
                 </div>
-                <div class="inline-flex items-center justify-center w-full px-6">
+                <div class="inline-flex items-center justify-center w-full">
                     <hr class="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
                     <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
                         or
                     </span>
                 </div>
-                <p class="px-6 text-center">Sign in with</p>
-                <div class="px-6 pb-6">
-                    <button  @click="signInWithFacebook" class="flex items-center justify-center w-full py-2 my-3 bg-transparent border-gray-300 hover:bg-indigo-200 hover:border-s-gray-100">
-                        <img src="/facebook-logo.svg" class="w-5" alt="">
-                        <b class="ml-2">Facebook</b>
-                    </button>
-                    <button @click="signInWithGoogle" class="flex items-center justify-center w-full py-2 bg-transparent border-gray-300 hover:bg-indigo-200 hover:border-s-gray-100">
-                        <img src="/google-logo.svg" class="w-5 " alt="">
-                        <b class="ml-2">Google</b>
-                    </button>
+                <p class="text-center">Sign in with</p>
+                <button  @click="signInWithFacebook" class="flex items-center justify-center w-full py-2 my-3 bg-transparent border-gray-300 hover:bg-indigo-200 hover:border-s-gray-100">
+                    <img src="/facebook-logo.svg" class="w-5" alt="">
+                    <b class="ml-2">Facebook</b>
+                </button>
+                <button @click="signInWithGoogle" class="flex items-center justify-center w-full py-2 bg-transparent border-gray-300 hover:bg-indigo-200 hover:border-s-gray-100">
+                    <img src="/google-logo.svg" class="w-5 " alt="">
+                    <b class="ml-2">Google</b>
+                </button>
                 </div>
             </div>
         </div>
     </div>
-    <div class="fixed inset-0 z-10 bg-gray-900 bg-opacity-50 dark:bg-opacity-80"></div>
 </template>
