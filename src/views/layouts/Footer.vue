c139fc6ecf8ec322e4b9e4429a8bcf82862a994c
Francis Canayon <canayonfrancis@gmail.com>
2023-08-08 22:31:38 +0800

    Change email button

--- src/views/layouts/Footer.vue
+++ src/views/layouts/Footer.vue
@@ -89,7 +89,7 @@
                                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                     </template>
                                     <template #suffix>
-                                    <Button>Join</Button>
+                                    <Button class="bg-main-color hover:bg-main-color">Join</Button>
                                     </template>
                                 </Input>
                             </div>
