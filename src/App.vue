b3b0ebd2f8223729586415910a39c096818cd1ab
Alvintooong <aeptechnologies@gmail.com>
2023-08-08 18:12:42 +0800

    Add preloading boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
   import Header from './views/layouts/Header.vue';
   import Footer from './views/layouts/Footer.vue';
   import SignIn from './views/sign/In.vue';
+  import { getCurrentUser } from './plugins/firebase';
 
   const showLogin = ref(false);
+  const loading = ref(true);
+
+  onMounted(() => {
+    user();
+  });
+
+  const user = async () => {
+    await getCurrentUser()
+            .then((res) => {
+              loading.value = false
+            })
+  }
+
 
 </script>
 <template>
+  <div class="preloader" v-if="loading">saf ds af dsf</div>
   <Header @login-is-click="showLogin = !showLogin" />
   <section class="min-h-screen">
     <router-view class="lg:container md:mx-auto"/>
