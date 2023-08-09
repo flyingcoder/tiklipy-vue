b1b0878b8c5a710d4421fa9858e94b2656ea00ab
Francis Canayon <canayonfrancis@gmail.com>
2023-08-09 17:27:41 +0800

    Add link for footer menu

--- src/views/layouts/MembersFooter.vue
+++ src/views/layouts/MembersFooter.vue
@@ -10,9 +10,15 @@
                 <div class="bottom-0 justify-between px-4 py-10 mx-16 font-light text-center md:flex md:items-center md:text-left" style="width: -webkit-fill-available">
                     <div class="mb-6 text-sm font-medium md:mb-0">© 2023 Tiklipy. All rights reserved.</div>
                     <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-                        <a href="#" class="block text-sm">Terms</a>
-                        <a href="#" class="block text-sm">Privacy</a>
-                        <a href="#" class="block text-sm">License</a>
+                        <router-link :to="{name: 'terms'}">
+                            <a class="block text-sm">Terms</a>
+                        </router-link>
+                        <router-link :to="{name: 'privacy'}">
+                            <a class="block text-sm">Privacy</a>
+                        </router-link>
+                        <router-link :to="{name: 'license'}">
+                            <a class="block text-sm">License</a>
+                        </router-link>
                     </div>
                 </div>
             </footer>
