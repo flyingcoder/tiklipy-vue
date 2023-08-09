1e08bc99b5091a0a5e3f0b80838b05804a5fa00a
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-08-09 20:52:46 +0800

    Fix mobile responsiveness

--- src/views/members/Dashboard.vue
+++ src/views/members/Dashboard.vue
@@ -4,11 +4,11 @@
 </script>
 
 <template>
-    <div class="mt-16">
+    <div class="mt-16 px-4">
         <div class="mb-16">
             <div class="block justify-center font-semibold">
                 <span class="text-purple-600 tracking-wider text-sm uppercase mt-0 mb-1 font-poppins font-bold leading-6">WELCOME BACK</span>
-                <div class="flex justify-between items-center">
+                <div class="flex flex-wrap justify-between items-center ">
                     <div>
                         <h2 class="text-gray-800 text-3xl mt-2 mb-3">Get Ready for the New School Year</h2>
                     </div>
@@ -35,9 +35,9 @@
                     </Breadcrumb>
                 </div>
             </div>
-            <div class="grid pt-10 grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
+            <div class="grid pt-10 grid-cols-1 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -52,7 +52,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -67,7 +67,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -83,7 +83,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -104,9 +104,9 @@
                 <span class="text-purple-600 tracking-wider text-sm uppercase mt-0 mb-1 font-poppins font-bold leading-6">ESSENTIALS</span>
                 <h2 class="text-gray-800 text-3xl mt-2">Focus on the Basics</h2>
             </div>
-            <div class="grid pt-10 grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
+            <div class="grid pt-10 grid-cols-1 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -121,7 +121,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -136,7 +136,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -151,7 +151,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -173,9 +173,9 @@
                 <span class="text-purple-600 tracking-wider text-sm uppercase mt-0 mb-1 font-poppins font-bold leading-6">TRY SOMETHING NEW</span>
                 <h2 class="text-gray-800 text-3xl mt-2">Shortcuts You Should Know About</h2>
             </div>
-            <div class="grid pt-10 grid-cols-4 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
+            <div class="grid pt-10 grid-cols-1 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -190,7 +190,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -205,7 +205,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -220,7 +220,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md bg-[url('/public/p-1.png')] bg-no-repeat bg-contain" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
@@ -235,7 +235,7 @@
                     </div>
                 </the-card>
                 <the-card href="#" class="w-full border-none rounded-lg shadow-none hover:bg-white hover:shadow-md" style="max-width: 100% !important">
-                    <div class="p-6 dark:bg-gray-800 dark:border-gray-700">
+                    <div class="md:p-6 dark:bg-gray-800 dark:border-gray-700">
                         <!-- <svg class="w-7 h-7 mb-9  text-main-color dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
                         </svg> -->
