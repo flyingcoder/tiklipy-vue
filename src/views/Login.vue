389a7ee79ac5820f5e1b1e4657d769df018d01f9
Alvintooong <aeptechnologies@gmail.com>
2023-08-12 01:31:15 +0800

    Redirect if already login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <script setup>
-    import { ref } from "vue"
-    import { useRouter } from "vue-router"
+    import { onMounted, ref } from "vue";
+    import { useRouter } from "vue-router";
+    import { getCurrentUser } from "../plugins/firebase";
     import { getAuth, 
         signInWithEmailAndPassword,
         GoogleAuthProvider,
@@ -13,8 +14,17 @@
     const password = ref("");
     const errMsg = ref();
     const isLoading = ref(false);
-    const hasError = ref(false)
-    const wrongCred = ref(false)
+    const hasError = ref(false);
+    const wrongCred = ref(false);
+
+    onMounted(async () => {
+        await getCurrentUser()
+                .then((user) => {
+                    if(user) {
+                        router.push({ name: 'dashboard'})
+                    }
+                });
+    })
     
     const signIn = async () => {
         isLoading.value = true;
@@ -89,7 +99,7 @@
                         <input type="password" v-model="password" id="password" :class="hasError||wrongCred ? 'border-red-500':''" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a password">
                     </div>
                 </div>
-                <button @click="signIn"  :disabled="!email || !password" :class="[(!email || !password) || isLoading ? 'cursor-not-allowed !bg-gray-500 hover:bg-gray-500' : 'bg-main-color hover:bg-secondary-color', hasError?'animate__hinge z-10':'']" class="w-full px-4 py-2 mt-2 font-bold text-white bg-main-color rounded hover:bg-secondary-color">
+                <button @click="signIn"  :disabled="!email || !password" :class="[(!email || !password) || isLoading ? 'cursor-not-allowed !bg-gray-500 hover:bg-gray-500' : 'bg-main-color hover:bg-secondary-color', hasError?'animate__hinge z-10':'']" class="w-full px-4 py-2 mt-2 font-bold text-white rounded bg-main-color hover:bg-secondary-color">
                     {{ isLoading ? "Loading..." : "Login" }}
                 </button>
             </div>
