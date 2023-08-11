7f48f63574b76054d73d1843c7abd4c8227594dd
Francis Canayon <canayonfrancis@gmail.com>
2023-08-11 19:36:27 +0800

    Add visual aid content

--- src/views/members/Visuals.vue
+++ src/views/members/Visuals.vue
@@ -2,5 +2,39 @@
 
 </script>
 <template>
-    
+    <div class="mt-7 px-3 flex bg-white rounded-xl">
+        <div class="w-1/4 p-5 custom-pulse">
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+            <div class="lg:h-48 flex bg-gray-300 md:h-36 w-full object-cover object-center rounded-lg mb-4">
+                <h2 class="bg-gray-400 h-10 w-10 my-2 ml-2 rounded-full"></h2>
+            </div>
+        </div>
+        <div class="w-3/4 p-5">
+            <div class="h-[51rem] bg-gray-300 w-full block object-cover object-center rounded-lg p-4 px-16">
+                <p class="leading-relaxed mb-3 h-14 w-[38rem] mt-8 mx-auto bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 h-10 w-[50rem] mt-8 mx-auto bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-8/12 h-7 mt-20 bg-gray-400 mr-0 mx-auto"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-14  bg-gray-400"></p>
+            </div>
+        </div>
+    </div>
 </template>
