1c001ac68b19b4a7c30d2aa327178e27ec96323d
Francis Canayon <canayonfrancis@gmail.com>
2023-08-11 22:03:50 +0800

    Add visuals aid content

--- src/views/members/Visuals.vue
+++ src/views/members/Visuals.vue
@@ -1,5 +1,28 @@
 <script setup>
+    import { ref, onMounted } from 'vue';
+    import Swal from 'sweetalert2';
+    import 'sweetalert2/dist/sweetalert2.min.css';
+    import { useRouter } from "vue-router"
 
+    const router = useRouter();
+
+    onMounted(() => {
+        showAlert();
+    });
+
+    const showAlert = () => {
+        Swal.fire({
+            title: 'Visual Aid Feature',
+            text: 'This functionality is currently under development and remains in progress. We will duly inform you as soon as this feature becomes accessible.',
+            icon: 'info',
+            allowOutsideClick: false,
+            confirmButtonText: 'Certainly!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                router.push({ name: 'dashboard' })
+            }
+        });;
+    };
 </script>
 <template>
     <div class="mt-7 px-3 flex bg-white rounded-xl">
@@ -24,16 +47,16 @@
             </div>
         </div>
         <div class="w-3/4 p-5">
-            <div class="h-[51rem] bg-gray-300 w-full block object-cover object-center rounded-lg p-4 px-16">
-                <p class="leading-relaxed mb-3 h-14 w-[38rem] mt-8 mx-auto bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 h-10 w-[50rem] mt-8 mx-auto bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-8/12 h-7 mt-20 bg-gray-400 mr-0 mx-auto"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-8  bg-gray-400"></p>
-                <p class="leading-relaxed mb-3 w-full h-7 mt-14  bg-gray-400"></p>
+            <div class="h-[51rem] bg-gray-300 w-full block object-cover object-center rounded-lg p-4 px-16 custom-pulse">
+                <p class="leading-relaxed mb-3 h-14 w-[38rem] mt-8 mx-auto bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 h-10 w-[50rem] mt-8 mx-auto bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-8/12 h-7 mt-20 bg-gray-400 mr-0 mx-auto rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-full h-7 mt-8 bg-gray-400 rounded-md"></p>
+                <p class="leading-relaxed mb-3 w-1/3 h-7 mt-20 bg-gray-400 rounded-md"></p>
             </div>
         </div>
     </div>
