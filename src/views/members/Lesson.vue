3224377d4b60efe530cf84e20e03c7ce5cc17577
Alvintooong <aeptechnologies@gmail.com>
2023-08-13 07:46:16 +0800

    initialized lessons api

--- src/views/members/Lesson.vue
+++ src/views/members/Lesson.vue
@@ -1,5 +1,4 @@
 <script setup>
-    import { Breadcrumb, BreadcrumbItem } from 'flowbite-vue'
     import {ref} from 'vue';
     import LessonPlan from '../../components/LessonPlan.vue';
     import Activities from '../../components/Activities.vue';
@@ -20,17 +19,17 @@
 
 </script>
 <template>
-    <div class="mt-7 px-3">
+    <div class="px-3 mt-7">
         <div class="flex flex-wrap justify-between mb-6">
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('lessonPlan')">
                 <div class="bg-white dark:bg-slate-800 shadow  rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/docs.png" width="60" alt="">                     
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">Lesson Plan</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated Lesson Plan</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">Lesson Plan</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated Lesson Plan</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
@@ -38,12 +37,12 @@
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('ppt')">
                 <div class="bg-white dark:bg-slate-800 shadow  rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/slide.png" width="30" alt="">                       
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">PowerPoint</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated PowerPoint</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">PowerPoint</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated PowerPoint</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
@@ -51,12 +50,12 @@
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('quiz')">
                 <div class="bg-white dark:bg-slate-800 shadow  rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/docs.png" width="60" alt="">                      
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">Quiz</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated Quiz</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">Quiz</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated Quiz</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
@@ -64,12 +63,12 @@
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('homework')">
                 <div class="bg-white dark:bg-slate-800 shadow rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/docs.png" width="60" alt="">                     
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">Homework</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated Homework</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">Homework</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated Homework</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
@@ -77,12 +76,12 @@
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('activities')">
                 <div class="bg-white dark:bg-slate-800 shadow rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/docs.png" width="60" alt="">                     
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">Activities</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated Activities</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">Activities</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated Activities</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
@@ -90,12 +89,12 @@
             <div class="w-full sm:w-[49%] lg:w-[32%] cursor-pointer mb-3" @click="setActiveView('projects')">
                 <div class="bg-white dark:bg-slate-800 shadow rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
                     <div class="flex justify-between xl:gap-x-2 items-cente">
-                        <div class="absolute -left-6 -top-4 text-blue-500 p-3 text-center inline-flex items-center justify-center w-32 h-32 ">
+                        <div class="absolute inline-flex items-center justify-center w-32 h-32 p-3 text-center text-blue-500 -left-6 -top-4 ">
                             <img src="/docs.png" width="60" alt="">                     
                         </div>
                         <div class="self-center ml-auto text-right">                            
-                            <h3 class="my-1 font-semibold text-2xl text-main-color">Projects</h3>
-                            <p class="text-gray-700 mb-0 text-lg font-light bg-transparent p-0 hover:border-transparent focus:outline-none">Generated Projects</p>
+                            <h3 class="my-1 text-2xl font-semibold text-main-color">Projects</h3>
+                            <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Generated Projects</p>
                         </div>
                     </div>
                 </div> <!--end inner-grid--> 
