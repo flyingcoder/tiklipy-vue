95ca51a02995adf0534fe500a5d1e08c4103f558
Francis Canayon <canayonfrancis@gmail.com>
2023-08-14 04:47:41 +0800

    Add generate form

---
+++ src/components/GenerateForm.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        Type: String,
+    });
+</script>
+<template>
+    <div class="w-full">
+        <div class="bg-gray-100 p-6 rounded-3xl">
+        <h2 class="text-2xl font-bold text-gray-800 leading-snug mb-10">Enter Your Upcoming Lesson</h2>
+            <div class="mb-4">
+            <label class="text-gray-700 font-semibold mb-1 block">Select Grade level</label>
+            <div class="mb-7 bg-transparent-input">
+                <select class="w-full text-sm bg-gray-50 border-[1px] border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 p-2.5">
+                <option disabled="" value="">Please select Grade level</option>
+                <option value="1">Grade 1</option>
+                <option value="2">Grade 2</option>
+                </select>
+            </div>
+            </div>
+
+            <div class="mb-4">
+            <label class="text-gray-700 font-semibold mb-1 block">Select Subject</label>
+            <div class="mb-7 bg-transparent-input">
+                <select class="w-full text-sm bg-gray-50 border-[1px] border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 p-2.5">
+                <option disabled="" value="">Please select Subject</option>
+                <option value="Math">Math</option>
+                <option value="Science">Science</option>
+                </select>
+            </div>
+            </div>
+
+            <!-- Topic Input -->
+            <div class="mb-4">
+            <label for="" class="text-gray-700 font-semibold mb-1 block">Topic</label>
+            <input type="text" id="text" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-main-color focus:border-main-color block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-main-color dark:focus:border-main-color dark:shadow-sm-light" placeholder="Lesson Topic" required="">
+            </div>
+
+            <div class="mb-4">
+            <div class="flex items-center mb-6">
+                <label class="mr-3 text-gray-700 font-semibold">Assessment</label>
+                <label class="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" class="sr-only peer" value="">
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-secondary-color dark:peer-focus:ring-secondary-color dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-main-color"></div>
+                </label>
+            </div>
+            </div>
+
+            <div class="mb-6">
+            <div class="flex items-center mb-6">
+                <label class="mr-3 text-gray-700 font-semibold">Homework</label>
+                <label class="relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" class="sr-only peer" value="">
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-secondary-color dark:peer-focus:ring-secondary-color dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-main-color"></div>
+                </label>
+            </div>
+            </div>
+
+            <div class="mb-6">
+            <label>
+                <div class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</div>
+                <div class="">
+                <textarea rows="4" class="border p-2.5 w-full text-sm bg-gray-50 rounded-lg border-gray-200 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Additional instruction..."></textarea>
+                </div>
+            </label>
+            </div>
+
+            <button class="text-white bg-blue-700 focus:ring-4 font-medium rounded-lg dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800 hover:bg-blue-800 dark:hover:bg-blue-700 px-5 py-2.5 w-full border-0 lg:text-[0.775rem] xl:text-lg text-white bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800 hover:bg-blue-800 dark:hover:bg-blue-700 text-base px-5 py-2.5 w-full bg-main-color hover:bg-secondary-color border-0 text-sm lg:text-[0.775rem] xl:text-lg font-semibold" type="submit">Generate Topic with Tiklipy!</button>
+        </div>
+    </div>
+</template>
--- src/components/LessonPlan.vue
+++ src/components/LessonPlan.vue
@@ -1,125 +1,144 @@
 <script setup>
+    import { defineProps } from 'vue';
+//import generate form
+    import GenerateForm from './GenerateForm.vue';
+//set props
+    const props = defineProps({
+        Type: String,
+    });
+  
+//emit and emit handler
+//sa generate form component 
+// define props
+// define emit action
 </script>
 <template>
-    <div class="generated-lesson-plan">
-        <header class="mb-[2rem]">
-            <h1 class="font-bold text-3xl font-bold">Science Lesson Plan</h1>
-            <h2 class="text-lg">Introduction to Photosynthesis</h2>
-        </header>
+    <div class="flex flex-wrap">
+        <div class="w-full lg:w-[68%] col-span-4">
+            <div class="generated-lesson-plan">
+                <header class="mb-[2rem]">
+                    <h1 class="font-bold text-3xl font-bold">Science Lesson Plan</h1>
+                    <h2 class="text-lg">Introduction to Photosynthesis</h2>
+                </header>
 
-        <section class="mb-[2rem]">
-            <div class="bg-red-100 p-6 rounded-lg ">
-                <h3 class=""><strong>Objective</strong></h3>
-                <p>
-                    By the end of this lesson, students will be able to understand the process of photosynthesis, identify its
-                    key components, and explain the importance of photosynthesis in the ecosystem.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-red-100 p-6 rounded-lg ">
+                        <h3 class=""><strong>Objective</strong></h3>
+                        <p>
+                            By the end of this lesson, students will be able to understand the process of photosynthesis, identify its
+                            key components, and explain the importance of photosynthesis in the ecosystem.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-green-100 p-6 rounded-lg ">
-                <h3><strong>Materials</strong></h3>
-                <ul>
-                    <li>Projector or whiteboard</li>
-                    <li>PowerPoint presentation or chalkboard</li>
-                    <li>Handouts with diagrams of photosynthesis</li>
-                    <li>Plant leaves (real or artificial) for a demonstration</li>
-                </ul>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-green-100 p-6 rounded-lg ">
+                        <h3><strong>Materials</strong></h3>
+                        <ul>
+                            <li>Projector or whiteboard</li>
+                            <li>PowerPoint presentation or chalkboard</li>
+                            <li>Handouts with diagrams of photosynthesis</li>
+                            <li>Plant leaves (real or artificial) for a demonstration</li>
+                        </ul>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-yellow-100 p-6 rounded-lg ">
-                <h3><strong>Introduction</strong></h3>
-                <p>
-                    Begin the lesson by asking students what they know about plants and their importance to the environment.
-                    Introduce the topic of photosynthesis as the process by which plants convert sunlight into energy and produce
-                    oxygen. Explain the objective of the lesson and what students will learn about photosynthesis.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-yellow-100 p-6 rounded-lg ">
+                        <h3><strong>Introduction</strong></h3>
+                        <p>
+                            Begin the lesson by asking students what they know about plants and their importance to the environment.
+                            Introduce the topic of photosynthesis as the process by which plants convert sunlight into energy and produce
+                            oxygen. Explain the objective of the lesson and what students will learn about photosynthesis.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-orange-100 p-6 rounded-lg ">
-                <h3><strong>Photosynthesis Explanation</strong></h3>
-                <p>
-                    Use a PowerPoint presentation or the chalkboard to explain the process of photosynthesis step by step.
-                    Cover the following points:
-                </p>
-                <ul  class="list-disc ml-5">
-                    <li>Definition of photosynthesis</li>
-                    <li>The role of chloroplasts in plants</li>
-                    <li>The reactants of photosynthesis (carbon dioxide, water, sunlight)</li>
-                    <li>The products of photosynthesis (glucose, oxygen)</li>
-                    <li>The chemical equation of photosynthesis</li>
-                    <li>Importance of photosynthesis in the ecosystem</li>
-                </ul>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-orange-100 p-6 rounded-lg ">
+                        <h3><strong>Photosynthesis Explanation</strong></h3>
+                        <p>
+                            Use a PowerPoint presentation or the chalkboard to explain the process of photosynthesis step by step.
+                            Cover the following points:
+                        </p>
+                        <ul  class="list-disc ml-5">
+                            <li>Definition of photosynthesis</li>
+                            <li>The role of chloroplasts in plants</li>
+                            <li>The reactants of photosynthesis (carbon dioxide, water, sunlight)</li>
+                            <li>The products of photosynthesis (glucose, oxygen)</li>
+                            <li>The chemical equation of photosynthesis</li>
+                            <li>Importance of photosynthesis in the ecosystem</li>
+                        </ul>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-teal-100 p-6 rounded-lg ">
-                <h3><strong>Interactive Activity</strong></h3>
-                <p>
-                    Divide the students into groups of 3-4. Distribute handouts with diagrams of the photosynthesis process.
-                    Instruct each group to label the different components of photosynthesis in the diagram and discuss the
-                    significance of each step. Walk around the class to provide guidance and answer any questions.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-teal-100 p-6 rounded-lg ">
+                        <h3><strong>Interactive Activity</strong></h3>
+                        <p>
+                            Divide the students into groups of 3-4. Distribute handouts with diagrams of the photosynthesis process.
+                            Instruct each group to label the different components of photosynthesis in the diagram and discuss the
+                            significance of each step. Walk around the class to provide guidance and answer any questions.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-blue-100 p-6 rounded-lg ">
-                <h3><strong>Demonstration</strong></h3>
-                <p>
-                    Conduct a live demonstration of photosynthesis using plant leaves (real or artificial). Show how the leaves
-                    absorb sunlight and explain the role of chlorophyll in capturing energy. Discuss how the plant releases
-                    oxygen during the process.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-blue-100 p-6 rounded-lg ">
+                        <h3><strong>Demonstration</strong></h3>
+                        <p>
+                            Conduct a live demonstration of photosynthesis using plant leaves (real or artificial). Show how the leaves
+                            absorb sunlight and explain the role of chlorophyll in capturing energy. Discuss how the plant releases
+                            oxygen during the process.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-red-100 p-6 rounded-lg ">
-                <h3><strong>Review and Discussion</strong></h3>
-                <p>
-                    Recap the main points of the lesson on photosynthesis. Ask students to share what they found most interesting
-                    or challenging about the topic. Address any misconceptions or questions that may have arisen during the lesson.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-red-100 p-6 rounded-lg ">
+                        <h3><strong>Review and Discussion</strong></h3>
+                        <p>
+                            Recap the main points of the lesson on photosynthesis. Ask students to share what they found most interesting
+                            or challenging about the topic. Address any misconceptions or questions that may have arisen during the lesson.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-green-100 p-6 rounded-lg ">
-                <h3><strong>Homework (optional)</strong></h3>
-                <p>
-                    Assign a short research task for students to find examples of photosynthetic organisms other than plants,
-                    such as algae or certain bacteria.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-green-100 p-6 rounded-lg ">
+                        <h3><strong>Homework (optional)</strong></h3>
+                        <p>
+                            Assign a short research task for students to find examples of photosynthetic organisms other than plants,
+                            such as algae or certain bacteria.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-yellow-100 p-6 rounded-lg ">
-                <h3><strong>Assessment</strong></h3>
-                <p>
-                    Observe students' participation during the interactive activity and discussion to assess their understanding
-                    of photosynthesis. Review their handouts to gauge their ability to label and explain the components of the
-                    process.
-                </p>
-            </div>
-        </section>
+                <section class="mb-[2rem]">
+                    <div class="bg-yellow-100 p-6 rounded-lg ">
+                        <h3><strong>Assessment</strong></h3>
+                        <p>
+                            Observe students' participation during the interactive activity and discussion to assess their understanding
+                            of photosynthesis. Review their handouts to gauge their ability to label and explain the components of the
+                            process.
+                        </p>
+                    </div>
+                </section>
 
-        <section class="mb-[2rem]">
-            <div class="bg-orange-100 p-6 rounded-lg ">
-                <h3><strong>Extension Activities</strong></h3>
-                <p>
-                    For students who show a strong grasp of the topic, encourage them to research more about the impact of
-                    photosynthesis on climate change and its role in maintaining the Earth's ecological balance. They can create
-                    presentations or essays on this subject.
-                </p>
+                <section class="mb-[2rem]">
+                    <div class="bg-orange-100 p-6 rounded-lg ">
+                        <h3><strong>Extension Activities</strong></h3>
+                        <p>
+                            For students who show a strong grasp of the topic, encourage them to research more about the impact of
+                            photosynthesis on climate change and its role in maintaining the Earth's ecological balance. They can create
+                            presentations or essays on this subject.
+                        </p>
+                    </div>
+                </section>
             </div>
-        </section>
+        </div>
+        <div class="w-full lg:w-[30%] col-span-2">
+            <GenerateForm :Type="Type" />
+        </div>
     </div>
 </template>
