e52a1cb19ab955a94dea918b7ef113fb30f9beac
Francis Canayon <canayonfrancis@gmail.com>
2023-08-13 20:38:27 +0800

    Create 404 page and add redirection function

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+    <div class="text-black">
+        <h1>404 - Page Not Found</h1>
+        <p>The page you're looking for doesn't exist.</p>
+    </div>
+</template>
