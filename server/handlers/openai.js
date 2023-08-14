794f15ba4eea7d327054ae301318053e9d5267cb
Alvintooong <aeptechnologies@gmail.com>
2023-08-14 21:49:55 +0800

    Create generateModel class for openai request

---
+++ server/handlers/openai.js
@@ -0,0 +1,32 @@
+import openai from './../plugins/openai-handler.js';
+
+class generateModel {
+    constructor() {
+        this.options = {
+            model: "gpt-3.5-turbo",
+            temperature: 0,
+            max_tokens: 500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        };
+    }
+
+    async lessonPlan(req, res) {
+        try {
+            const param = { 
+                messages: req.body,
+                ...this.options
+            };
+    
+            const chatCompletion = await openai.createChatCompletion(param)
+                                        .then(res => res.data);
+    
+            res.json(chatCompletion.choices.pop());
+        } catch (error) {
+            res.json({'error': error});
+        }
+    }
+}
+
+export default generateModel;
