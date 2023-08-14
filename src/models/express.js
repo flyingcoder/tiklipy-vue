6349fc8212fb18b29116902bc99ca1dbfc71aaf6
Alvintooong <aeptechnologies@gmail.com>
2023-08-14 21:49:14 +0800

    generateLesson that will call backend to request for openai response

---
+++ src/models/express.js
@@ -0,0 +1,24 @@
+import axios from "../plugins/axios";
+import { auth } from "../plugins/firebase";
+
+class expressModel {
+    constructor() {
+        axios.defaults.headers.common['Authorization'] = auth?.currentUser?.accessToken;
+    }
+
+    async generateLesson(instruction) {
+        try {
+            const params = [{role: 'user',content: instruction}];
+            const lesson = null;
+            await axios.post('/api/v1/lesson', params)
+                .then((completion) => { lesson = completion.data.message })
+            return lesson;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+        
+    }
+}
+
+export default expressModel;
