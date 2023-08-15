9c3bbc562f440a2b99c9016b729911f3259a59e3
Alvintooong <aeptechnologies@gmail.com>
2023-08-15 12:56:58 +0800

    Add update star credit function in API

--- server/models/StarCredit.js
+++ server/models/StarCredit.js
@@ -1,10 +1,20 @@
+import { getFirestore } from "firebase-admin/firestore";
 
 class StarCreditModel {
     constructor() {
-
+        const db = getFirestore();
+        this.col = db.collection('customers');
     }
-    async fetchStarCount() {
 
+    async updateStarCredits(userId, newCredits) {
+        try {
+            const userDocRef = this.col.doc(userId);
+            await userDocRef.update({ starCredits: newCredits });
+            return true;
+        } catch (error) {
+            console.error("StarCreditModel.updateStarCredits error:", error);
+            return false;
+        }
     }
 }
 
