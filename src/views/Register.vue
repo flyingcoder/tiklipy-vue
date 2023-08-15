70501fa3532b986f884ac5f12de58d66029601fd
Alvintooong <aeptechnologies@gmail.com>
2023-08-15 15:14:44 +0800

    Add registration route

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -22,9 +22,7 @@
     onMounted(() => {
         
     });
-    const forgotPassword = () => {
-        // forgot password listener
-    }
+
     const loginVia = async (provider) => {
         loaderStore.toggle();
         const success = await authStore.loginVia(provider);
@@ -32,9 +30,9 @@
         loaderStore.toggle();
     }
 
-    const login = async () => {
+    const register = async () => {
         loaderStore.toggle();
-        const success = await authStore.login(email.value, password.value);
+        const success = await authStore.register(email.value, password.value);
         if (!success) {
             loginFail();
             hasError.value = true;
@@ -104,14 +102,14 @@
                 <router-link :to="{ name: 'home' }">
                     <img class="w-32 mx-auto mb-4" src="/tiklipy-logo-indigo.png" alt="tiklipy logo indigo color">
                 </router-link>
-                <h2 class="w-full text-2xl font-semibold text-center">Sign in to Start!</h2>
-                <p class="text-lg font-semibold text-center">
-                    Start generating fantastic lessons!
+                <h2 class="w-full text-2xl font-semibold text-center">Everyone is joining!</h2>
+                <p class="text-lg text-center">
+                    Become part of the new era of teaching!
                 </p>
             </div>
 
             <div class="items-center justify-center w-full px-6 text-black">
-                <p :class="hasError ? 'hidden':'visible'" class="mt-4 text-sm font-semibold text-center">Login with Email & Password</p>
+                <p :class="hasError ? 'hidden':'visible'" class="mt-4 text-sm font-semibold text-center">Register with Email & Password</p>
                 <p :class="hasError ? 'visible':'hidden'" class="mt-4 text-sm font-semibold text-center text-red-500">Invalid Email or Password</p>
                 <div class="my-2">
                     <div>
@@ -125,16 +123,13 @@
                         <input type="password" v-model="password" id="password" :class="hasError||wrongCred ? 'border-red-500':''" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-main-color focus:border-main-color block w-full p-2.5 dark:border-gray-600 dark:text-white dark:focus:ring-main-color dark:focus:border-main-color" placeholder="Enter a password">
                     </div>
                 </div>
-                <button @click="login"  :disabled="!email || !password" :class="(!email || !password) || isLoading ? 'cursor-not-allowed !bg-gray-500 hover:bg-gray-500' : 'bg-main-color hover:bg-secondary-color border-0'" class="w-full px-4 py-2 mt-2 font-bold text-white rounded bg-main-color hover:bg-secondary-color">
-                    {{ isLoading ? "Loading..." : "Login" }}
+                <button @click="register"  :disabled="!email || !password" :class="(!email || !password) || isLoading ? 'cursor-not-allowed !bg-gray-500 hover:bg-gray-500' : 'bg-main-color hover:bg-secondary-color border-0'" class="w-full px-4 py-2 mt-2 font-bold text-white rounded bg-main-color hover:bg-secondary-color">
+                    {{ isLoading ? "Loading..." : "Register" }}
                 </button>
-                <router-link :to="{ name: 'pricing' }">
-                    <button class="w-full px-4 py-2 mt-2 font-bold text-white rounded bg-main-color hover:bg-secondary-color">
-                        Sign up
-                    </button>
-                </router-link>
-                <p @click="forgotPassword" class="mt-4 text-sm font-semibold text-center ">
-                    <a href="#" class="text-black hover:underline">Forgot Password?</a>
+                <p>Already have an account?
+                    <router-link :to="{name: 'login'}">
+                        Login Here.
+                    </router-link>
                 </p>
             </div>
             <div class="inline-flex items-center justify-center w-full px-6">
@@ -143,7 +138,7 @@
                 or
                 </span>
             </div>
-            <p class="font-semibold text-center text-black">Sign in with</p>
+            <p class="font-semibold text-center text-black">Signup with</p>
             <div class="flex px-6 text-black">
                 <button v-if="false" class="hover:!border-secondary-color flex items-center justify-center w-full py-2 mt-3 bg-transparent border-gray-300 focus:border-gray-300 focus:outline-none">
                     <img src="/facebook-logo.svg" class="w-5" alt="">
