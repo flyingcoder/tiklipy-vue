fe005c16bc8eb0b381eb312518bb43266361c9ec
Francis Canayon <canayonfrancis@gmail.com>
2023-08-15 20:16:33 +0800

    Add filter for assessments pages

--- src/views/members/Projects.vue
+++ src/views/members/Projects.vue
@@ -1,8 +1,46 @@
 <script setup>
-    import { Breadcrumb, BreadcrumbItem } from 'flowbite-vue'
+    import { onMounted, ref } from 'vue';
+    import LessonPlanModel from '../../models/lessonPlans';
+    import { useAuthStore } from '../../stores/auth';
+    import Filter from '../../components/Filter.vue';
+
+
+    const authStore = useAuthStore();
+    const teacher = authStore.user;
+    const lessonModel = new LessonPlanModel();
+    const lessons = ref([]);
+
+    onMounted(async () => {
+        lessons.value = await lessonModel.getLessonPlansByTeacher(teacher.uid);
+    });
 </script>
 <template>
-    <div class="mt-7 px-3">
+    <div class="px-3 mt-7 flex flex-wrap justify-between">
+        <div class="flex flex-wrap max-lg:justify-center w-full md:w-3/4 lg h-fit">
+            <div v-for="lesson in lessons" :key="lesson.teacherId + '-lesson-card'" class="w-full sm:w-[47%] bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain ">
+                <router-link :to="{name: 'lesson', params: { id: lesson.slug }}" class=" ">
+                    <div class="p-10 max-xs:!p-5 ">
+                        <div class="w-full flex justify-between mb-10">
+                            <div class="text-md border px-2 rounded-md bg-green-500 text-white">English</div>
+                            <div class="flex items-center justify-end w-full">
+                                <img class="w-6 ml-2" src="/grade.png" alt="">
+                                <div class="text-lg">{{ lesson.gradeLevel }}</div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col justify-between leading-normal">
+                            <h5 class=" line-clamp-2 wh mb-2 text-lg lg:text-3xl font-bold tracking-tight text-main-color dark:text-white">
+                                {{ lesson.topic }}
+                            </h5>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+        <div class="form w-full md:w-1/4">
+            <Filter filter-title="Projects"/>
+        </div>
+    </div>
+    <!-- <div class="mt-7 px-3">
         <div class="flex flex-wrap max-lg:justify-center">
             <div class=" w-[30rem] bg-white rounded-lg sm:mr-5 mb-5 flex">
                 <router-link :to="{ name: 'assessment', params: { type: 'projects', id: 1 } }" class="bg-[url('/p-1.png')] bg-no-repeat bg-contain items-center bg-white rounded-lg  md:flex-row md:max-w-xl hover:shadow-lg transition delay-75 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
@@ -47,5 +85,5 @@
                 </router-link>
             </div>
         </div>
-    </div>
+    </div> -->
 </template>
