0aaa8d6d016ff3b1654bdfc18a19fd216d80bc28
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-08-17 01:57:13 +0800

    added cards

--- src/temp/alanjr.js
+++ src/temp/alanjr.js
@@ -1,20 +1,225 @@
 export default [
     {
-        type: 'auxiliary',
-        category: 'report',
-        tag: 'featured',
-        promptExample: 'Computer Laborator Supervisor Auxiliary report.',
-        systemPrompt: 'Assist in generating clear and concise instructions for monthly auxiliary reports. Provide guidance on structuring the report, including key points, challenges, highlights, and insights. Ensure the instructions are professional and informative to simplify the reporting process.',
-        title: 'Bayot 1',
-        icon:'ti-user',
-        description: 'Simplify your auxiliary assignment. Just share a few details, and Tiklipy will solve your problem.',
+        type: 'correction',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: 'She no went to class.',
+        systemPrompt: 'You will be provided with statements, and your task is to convert them to standard English.',
+        title: 'Grammar correction',
+        icon:'ti-tools',
+        description: 'Rewrite incorrect sentences into proper English.',
         inputs: {
-            highlights: {
+            sentence: {
                 value: '',
-                placeholder: 'e.g. Upgraded software in computer lab for better functionality and implemented new security measures to protect lab equipment.',
+                placeholder: 'Write here',
                 inputType: 'textarea',
-                label: 'Main Highlights',
+                label: 'Write the the sentence you would like to check and correct',
             },
         }
-    }
+    },
+    {
+        type: 'summarize',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "Jupiter, the Solar System's largest planet, is fifth from the Sun. Despite having only one-thousandth of the Sun's mass, it surpasses the combined mass of all other planets. Known since ancient times, it's named after the Roman god Jupiter. Visible to the naked eye, it often outshines most celestial objects, ranking third in brightness after the Moon and Venus.",
+        systemPrompt: 'You will be given content and a grade level. Summarize the provided content for a student based on their grade level.',
+        title: 'Summarization',
+        icon: 'ti-tools',
+        description: 'Summarize a sentence to a specific grade level',
+        inputs: {
+            grade: {
+                value: '',
+                placeholder: 'e.g. 2nd Grade',
+                inputType: 'input',
+                label: 'Write the student\'s grade',
+            },
+            sentence: {
+                value: '',
+                label: 'Sentence',
+                placeholder: "white here",
+                inputType: 'textarea',
+                rows: '10',
+            },
+        }
+    },
+    {
+        type: 'arrange',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: 'On the recently discovered planet Goocrux, a variety of unique fruits have been found. Neoskizzles are purple and taste like candy. Loheckles are grayish-blue and tart, similar to lemons. Pounits, bright green, have a savory flavor. Loopnovas are neon pink and taste like cotton candy. Lastly, glowls, with a pale orange hue, offer a sour and bitter taste.',
+        systemPrompt: 'You will be provided with unstructured data, and your task is to parse it into CSV format.',
+        title: 'Organize and understand data that is not structured in a specific format.',
+        icon:'ti-tools',
+        description: 'Create and organize tables from sentences.',
+        inputs: {
+            organize: {
+                value: '',
+                placeholder: 'Write here',
+                inputType: 'textarea',
+                label: 'Enter the sentence you would like to organize',
+                rows:'10',
+            },
+        }
+    },
+    {
+        type: 'translation',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: 'Artificial intelligence is a technology with great promise.',
+        systemPrompt: 'You will be provided with text, and your task is to translate it into emojis. Do not use any regular text. Do your best with emojis only.',
+        title: 'Emoji Translation',
+        icon:'ti-tools',
+        description: 'Translate regular text into emoji text.',
+        
+        inputs: {
+            translation: {
+                value: '',
+                placeholder: 'Write here',
+                inputType: 'textarea',
+                label: 'Enter the sentence you would like to translate',
+                rows:'10',
+            },
+        }
+    },
+    {
+        type: 'keyword',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "Black-on-black ware is a modern pottery tradition developed by Native American artists in Northern New Mexico. It's a style of pottery where designs are applied to smooth surfaces through burnishing or refractory slip. Some artists carve or incise designs and polish raised areas. Families from Kha'po Owingeh and P'ohwhóge Owingeh pueblos have passed down these techniques for generations. Other pueblos also create this pottery, and modern artists pay tribute to their ancestors' work.",
+        systemPrompt: "You will be provided with a block of text, and your task is to extract a list of keywords from it.",
+        title: 'Keywords Extraction',
+        icon:'ti-tools',
+        description: 'Extract keywords from a block of text.',
+        inputs: {
+            extractor: {
+                value: '',
+                placeholder: 'Write here',
+                inputType: 'textarea',
+                label: "Please provide the sentence from which you'd like to extract keywords.",
+                rows:'10',
+            },
+        }
+    },
+    {
+        type: 'spreadsheet_creator',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "First name, Last name, age, score",
+        systemPrompt: "You will be provided with block of text and the Data Fields, and your task is to create spreadsheet based on the text and the Data Fields",
+        title: 'Spreadsheet Creator',
+        icon:'ti-tools',
+        description: 'Create spreadsheets of various kinds of data.',
+        inputs: {
+            cols: {
+                value: '',
+                placeholder: ' First name, Last name, age, score, grade',
+                inputType: 'input',
+                label: 'Enter Information Overview',
+            },
+            data: {
+                value: '',
+                placeholder:'e.g. Aurora Sullivan, aged 25, achieved a score of 92; Jasper Bennett, aged 32, scored 78; Olivia Ramirez, aged 28, scored 85; Liam Anderson, aged 21, attained a score of 96; Sophia Mitchell, aged 29, received a score of 88; Ethan Roberts, aged 24, scored 91; Ava Martinez, aged 27, achieved a score of 83; Lucas Cooper, aged 30, scored 89; Mia Thompson, aged 22, received a score of 94; and Owen Johnson, aged 26, scored 77.',
+                inputType: 'textarea',
+                label: 'Enter data that you want to arrange',
+                rows:'10',
+            },
+        }
+    },
+    {
+        type: 'questions',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "5th grade, 5, volcano",
+        systemPrompt: "You will be provided with a block of text, a specified number of questions, and a designated grade level. Your task is to formulate the questions, and the number of questions as well as the complexity of the questions should align with the given grade level.",
+        title: 'Question generator',
+        icon:'ti-tools',
+        description: 'Create questions for spacific topic and grade level',
+        inputs: {
+            grade: {
+                value: '',
+                placeholder: 'e.g. 5th Grade',
+                inputType: 'input',
+                label: 'Enter the grade level',
+            },
+            questions: {
+                value: '',
+                placeholder: 'e.g. 5',
+                inputType: 'input',
+                label: 'Enter the number of questions',
+            },
+            topic: {
+                value: '',
+                placeholder:'e.g. volcano',
+                inputType: 'input',
+                label: 'Enter the topic',
+                rows:'10',
+            },
+        }
+    },
+    {
+        type: 'translation',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "English to 私は学生です",
+        systemPrompt: "You will be given a sentence and two languages in json format. Your task is to translate the sentence from its detected language to the second specified language.",
+        title: 'translation',
+        icon:'ti-tools',
+        description: 'Translate sentence to specified language.',
+        inputs: {
+            language: {
+                value: '',
+                placeholder: 'e.g. Bisaya',
+                inputType: 'input',
+                label: 'Enter the language you would like the translation to be in',
+            },
+            sentence: {
+                value: '',
+                placeholder: 'e.g. 私は学生です',
+                inputType: 'textarea',
+                label: 'Write the sentence here',
+            },
+        }
+    },
+    {
+        type: 'meeting_notes_summarizer',
+        category: 'tools',
+        tag: 'tools',
+        promptExample: "Examples below",
+        systemPrompt: "You will be provided with meeting notes, and your task is to summarize the meeting as follows: -Overall summary of discussion -Action items (what needs to be done and who is doing it) -If applicable, a list of topics that need to be discussed more fully in the next meeting.",
+        title: 'Meeting notes summarizer',
+        icon:'ti-tools',
+        description: 'Summarize meeting notes including overall discussion, action items, and future topics.',
+        inputs: {
+            date: {
+                value: '',
+                placeholder: 'e.g. March 5th, 2023',
+                inputType: 'input',
+                label: 'Enter the date',
+            },
+            time: {
+                value: '',
+                placeholder: 'e.g. 2:00 PM',
+                inputType: 'input',
+                label: 'Enter the time',
+            },
+            location: {
+                value: '',
+                placeholder: 'e.g. Conference Room 3B, Intergalactic Headquarters',
+                inputType: 'textarea',
+                label: 'Enter the location',
+            },
+            meeting: {
+                value: '',
+                placeholder: 'e.g. 1.Introductions and Welcome... etc',
+                inputType: 'textarea',
+                label: 'Write the agenda of the meeting',
+            },
+            nextMeeting: {
+                value: '',
+                placeholder: 'e.g. 1.Introductions and Welcome... etc',
+                inputType: 'textarea',
+                label: 'Write the agenda of the next meeting',
+            },
+        }
+    },
 ];
