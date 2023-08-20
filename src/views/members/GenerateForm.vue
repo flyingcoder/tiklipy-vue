2e981e94b9d566205a0d7658df4abeafca7cc9b1
Alvintooong <aeptechnologies@gmail.com>
2023-08-21 02:30:01 +0800

    Remove form hints and transfer it to the right side

--- src/views/members/GenerateForm.vue
+++ src/views/members/GenerateForm.vue
@@ -67,106 +67,45 @@
                         <i :class="formStore.icon" class="text-4xl text-white ti dark:text-white"></i>
                     </div>
                 </div>
-                <div class="self-center pl-4 text-2xl font-bold text-white">
+                <h1 class="self-center pl-4 text-2xl font-bold text-white">
                     {{ formStore.title }}
-                </div>
+                </h1>
             </div>
             <div class="flex flex-wrap gap-4 p-3 sm:p-9">
                 <div class="w-full lg:w-[68%] col-span-4">
                     <div class="w-full py-4 generated-value" v-if="!generatedResource">
-                        <h1 class="mb-6 text-2xl font-semibold">Creating Educational Content with Tiklipy in 9 Simple Steps:</h1>
+                        <h2 class="mb-6 text-2xl font-semibold">
+                            {{ formStore.description }}
+                        </h2>
                         <div class="space-y-6">
-                            <!-- Step 1 -->
                             <div>
-                                <h2 class="text-xl font-semibold">Step 1: Choose Lesson: Select grade, subject, topic.</h2>
-                            </div>
-
-                            <!-- Step 2 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 2: Assess & Homework: Toggle for tasks.</h2>
-                            </div>
-
-                            <!-- Step 3 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 3: Details: Set questions' specifics.</h2>
-                            </div>
-
-                            <!-- Step 4 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 4: Add Instructions: Extra guidance if needed.</h2>
-                            </div>
-
-                            <!-- Step 5 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 5: Generate: Click "Generate with Tiklipy!"</h2>
-                            </div>
-
-                            <!-- Step 6 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 6: Review & Interact: See content on the left.</h2>
-                            </div>
-
-                            <!-- Step 7 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 7: Copy/Save/Regenerate: Use buttons provided.</h2>
-                            </div>
-
-                            <!-- Step 8 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 8: Refine: Adjust choices, generate more.</h2>
-                            </div>
-
-                            <!-- Step 9 -->
-                            <div>
-                                <h2 class="text-xl font-semibold">Step 9: Follow Rubric: Check quality as per guidelines.</h2>
+                                <h2 class="text-xl">
+                                    Step 1: Choose Lesson: Select grade, subject, topic.
+                                </h2>
                             </div>
                         </div>
                     </div>
-                    <div class="w-full py-4 generated-value" v-html="generatedResource">
+                    <div class="w-full py-4 generated-value" v-if="generatedResource" v-html="generatedResource">
                         
                     </div>
-                    <div class="block">
+                    <div class="block" v-if="generatedResource">
                         <div class="flex">
-                            <Button color="default" @click="copyGeneratedTopic" class="py-3 mr-3 font-semibold border-0 bg-main-color hover:bg-secondary-color">
-                                <template #prefix>
-                                    <svg class="w-5 h-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7.708 2.292.706-.706A2 2 0 0 1 9.828 1h6.239A.97.97 0 0 1 17 2v12a.97.97 0 0 1-.933 1H15M6 5v4a1 1 0 0 1-1 1H1m11-4v12a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V9.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 5h5.239A.97.97 0 0 1 12 6Z"/>
-                                    </svg>
-                                </template>
-                                Copy
+                            <Button color="default" @click="copyGeneratedTopic" class="py-3 pr-5 mr-3 font-semibold uppercase border-0 bg-main-color hover:bg-secondary-color">
+                                <i class="mr-2 text-2xl align-middle ti ti-printer"></i>
+                                Print
                             </Button>
-                            <Button color="default" class="py-3 mr-3 font-semibold border-0 bg-main-color hover:bg-secondary-color">
-                                <template #prefix>
-                                    <svg class="w-5 h-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m13 19-6-5-6 5V2a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v17Z"/>
-                                    </svg>
-                                </template>
+                            <Button color="default" class="py-3 pr-5 mr-3 font-semibold uppercase border-0 bg-main-color hover:bg-secondary-color">
+                                <i class="mr-2 text-2xl align-middle ti ti-bookmark"></i>
                                 Save
                             </Button>
-                            <!-- <Button class="py-3 font-semibold border-0 bg-main-color hover:bg-secondary-color">
-                                <template #prefix>
-                                    <svg class="w-5 h-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3"/>
-                                    </svg>
-                                </template>
-                                Regenerate
-                            </Button> -->
                         </div>
                     </div>
                 </div>
                 <div class="w-full lg:w-[30%] col-span-2">
                     <div class="p-6 bg-gray-100 rounded-3xl animate__animated animate__fadeInUp">
                         <h3 class="mb-6 text-xl font-bold leading-snug text-black">
-                            {{ formStore.description }}
+                            Enhance results with more details.
                         </h3>
-                        <p v-if="formStore.promptExample" class="flex p-4 mb-6 text-purple-700 bg-blue-200 rounded-md">
-                            <div class="pr-3 w-14">
-                                <i class="text-purple-800 ti ti-writing-sign"></i>
-                            </div>
-                            <div class="">
-                                For Example: <br>{{ formStore.promptExample }}
-                            </div>
-                        </p>
                         <div v-for="(input, index) in formStore.inputs" :key="index + '-generate-form-input'" class="mb-4 font-semibold generate-form-section">
                             <Textarea :rows="input.rows" :placeholder="input.placeholder" v-model="formStore.inputs[index].value" v-if="input.inputType === 'textarea'" class="font-semibold" :label="input.label" />
                             <Input :placeholder="input.placeholder" v-model="formStore.inputs[index].value" v-else type="text" :label="input.label" class="font-semibold"/>
@@ -178,6 +117,5 @@
                 </div>
             </div> 
         </div>
-        
     </div>
 </template>
