d982f2b99889111a12d968cd6cf0d3a5908fbe93
Francis Canayon <canayonfrancis@gmail.com>
2023-08-22 07:35:47 +0800

    Redesign header filter

--- src/components/HeaderFilter.vue
+++ src/components/HeaderFilter.vue
@@ -27,7 +27,7 @@
                 </div>
                 <div class="flex self-center ml-auto text-right">
                     <div class="flex mr-5" v-if="route.path != '/members'">
-                        <select v-model="sortQuery" class="block text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <select v-model="sortQuery" class="block text-sm text-gray-900 border border-gray-300 rounded-full bg-white focus:ring-main-color focus:secondary-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="latest">Latest</option>
                             <option value="abc">Alphabetical</option>
                         </select>
@@ -36,17 +36,17 @@
                         <form>   
                             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div class="relative">
-                                <input v-model="searchQuery" type="search" id="default-search" class="block w-full p-4 pr-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="route.path === '/members' ? 'Search tools...' : 'Search generated card...'" required>
-                                <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                                <input v-model="searchQuery" type="search" id="default-search" class="block w-full py-3 px-4 pr-10 text-sm text-gray-900 font-semibold border border-gray-300 rounded-full bg-white focus:ring-main-color focus:border-secondary-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" :placeholder="route.path === '/members' ? 'Explore Search Tools...' : 'Search generated card...'" required>
+                                <div class="absolute inset-y-0 right-0 flex items-center pr-5 pointer-events-none">
                                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                     </svg>
                                 </div>
                             </div>
                         </form>
-                    </div>                    
+                    </div>               
                     <slot name="generate-btn">
-                        <button type="button" size="lg" v-if="route.path != '/members' && route.path != '/members/resources'" class="bg-main-color mr-5 hover:bg-secondary-color border-0 text-sm lg:text-[0.775rem] xl:text-base font-semibold">
+                        <button type="button" size="lg" v-if="route.path != '/members' && route.path != '/members/resources'" class="bg-main-color mr-5 hover:bg-secondary-color rounded-full border-0 text-sm lg:text-[0.775rem] xl:text-base font-semibold">
                             Generate
                         </button>
                     </slot>
