db1b2aaf95583f0a9cf7517a20945061e3e03330
Alvintooong <aeptechnologies@gmail.com>
2023-08-21 17:55:26 +0800

    Add margin on the generate button

--- src/components/HeaderFilter.vue
+++ src/components/HeaderFilter.vue
@@ -37,7 +37,7 @@
                             <option value="abc">Alphabetical</option>
                         </select>
                     </div> 
-                    <div :class="route.path !== '/members' ? 'w-[300px]' : 'w-[500px]'">
+                    <div class="mr-5" :class="route.path !== '/members' ? 'w-[300px]' : 'w-[500px]'">
                         <form>   
                             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div class="relative">
