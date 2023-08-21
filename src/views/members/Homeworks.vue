f5a032f6c7ac6447b34f3f6191a8ed8c4f12b66c
Francis Canayon <canayonfrancis@gmail.com>
2023-08-21 13:06:26 +0800

    Add component to each pages

--- src/views/members/Homeworks.vue
+++ src/views/members/Homeworks.vue
@@ -3,6 +3,7 @@
     import LessonPlanModel from '../../models/lessonPlans';
     import { useAuthStore } from '../../stores/auth';
     import { useFormStore } from '../../stores/form';
+    import HeaderFilter from '../../components/HeaderFilter.vue';
     import { useRouter } from 'vue-router';
 
 
@@ -79,30 +80,7 @@
         <div class="w-full flex gap-5">
             <div class="flex flex-wrap max-lg:justify-center w-full lg h-fit">
                 <div class="w-full">
-                    <div class="w-full sm:w-full bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain">
-                        <div class="bg-white dark:bg-slate-800 shadow  rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
-                            <div class="flex justify-between xl:gap-x-2 items-cente">
-                                <div class="absolute inline-flex items-center justify-center text-center text-main-color ">
-                                    <img src="/android-chrome-512x512.png" width="50" alt="">                       
-                                </div>
-                                <div class="ml-20">
-                                    <img src="/bongo-cat/coding-bongo-cat.gif" width="90" alt="">
-                                </div>
-                                <div class="self-center ml-auto text-right flex">
-                                    <div class="flex w-full mr-5">
-                                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                            <option value="US">Alphabetical</option>
-                                            <option value="CA">Grade Level</option>
-                                            <option value="FR">Date Created</option>
-                                        </select>
-                                    </div>
-                                    <Button type="submit" size="lg" class="w-full bg-main-color hover:bg-secondary-color border-0 text-sm lg:text-[0.775rem] xl:text-base font-semibold">Generate</Button>                      
-                                    <!-- <h3 class="my-1 text-2xl font-semibold text-main-color">Lesson Plan</h3>
-                                    <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Effective and Engaging Lesson Plans</p> -->
-                                </div>
-                            </div>
-                        </div> <!--end inner-grid--> 
-                    </div>
+                    <HeaderFilter/>
                     <div v-for="lesson in lessons" :key="lesson.teacherId + '-lesson-card'" class="w-full sm:w-[47%] bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain ">
                         <router-link :to="{name: 'lesson', params: { id: lesson.slug }}" class=" ">
                             <div class="p-10 max-xs:!p-5 ">
--- src/views/members/Quizes.vue
+++ src/views/members/Quizes.vue
@@ -3,6 +3,7 @@
     import LessonPlanModel from '../../models/lessonPlans';
     import { useAuthStore } from '../../stores/auth';
     import { useFormStore } from '../../stores/form';
+    import HeaderFilter from '../../components/HeaderFilter.vue';
     import { useRouter } from 'vue-router';
 
 
@@ -77,28 +78,8 @@
 <template>
      <div class="px-3 mt-7 flex flex-wrap justify-between">
         <div class="w-full flex gap-5">
-            <div class="flex flex-wrap max-lg:justify-center w-full md:w-full lg h-fit">
-                <div class="w-full sm:w-full bg-white rounded-lg mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain">
-                    <div class="bg-white dark:bg-slate-800 shadow  rounded-md w-full p-4 relative overflow-hidden bg-[url('/p-1.png')] bg-no-repeat bg-contain">
-                        <div class="flex justify-between xl:gap-x-2 items-cente">
-                            <div class="absolute inline-flex items-center justify-center text-center text-main-color ">
-                                <img src="/android-chrome-512x512.png" width="50" alt="">                       
-                            </div>
-                            <div class="self-center ml-auto text-right flex">
-                                <div class="flex w-full mr-5">
-                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option value="US">Alphabetical</option>
-                                        <option value="CA">Grade Level</option>
-                                        <option value="FR">Date Created</option>
-                                    </select>
-                                </div>
-                                <Button type="submit" size="lg" class="w-full bg-main-color hover:bg-secondary-color border-0 text-sm lg:text-[0.775rem] xl:text-base font-semibold">Generate</Button>                      
-                                <!-- <h3 class="my-1 text-2xl font-semibold text-main-color">Lesson Plan</h3>
-                                <p class="p-0 mb-0 text-lg font-light text-gray-700 bg-transparent hover:border-transparent focus:outline-none">Effective and Engaging Lesson Plans</p> -->
-                            </div>
-                        </div>
-                    </div> <!--end inner-grid--> 
-                </div>
+            <div class="flex flex-wrap max-lg:justify-center w-full lg h-fit">
+                <HeaderFilter/>
                 <div class="w-full">
                     <div v-for="lesson in lessons" :key="lesson.teacherId + '-lesson-card'"  class="w-full sm:w-wull bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain ">
                         <router-link :to="{name: 'lesson', params: { id: lesson.slug }}" class=" ">
