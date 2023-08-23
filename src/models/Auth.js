d3a85a304a7f11daa874c8e451fd6711f6de551c
Alvintooong <aeptechnologies@gmail.com>
2023-08-23 09:33:07 +0800

    Ga dali ko basta kay para na sa invitation code

---
+++ src/models/Auth.js
@@ -0,0 +1,40 @@
+import { collection, getDoc, doc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { db, auth } from "../plugins/firebase";
+import { useAuthStore } from "../stores/auth";
+
+
+class AuthModel {
+
+    constructor() {
+        this.colName = 'customers';
+        this.collection = collection(db, this.colName);
+    }
+
+    async getUser(uid) {
+        return await getDoc(doc(db, this.colName, uid)).then((snap) => snap.data());
+    }
+
+    async fireAuthState() {
+        const authStore = useAuthStore();
+        return new Promise((resolve) => {
+            const stop = onAuthStateChanged(auth, async (u) => {
+                 stop();
+                 if (u) {
+                     const cusDoc = await this.getUser(u.uid);
+                     const user = {
+                         uid : u.uid ?? null, email : u.email ?? null,
+                         displayName: u.displayName ?? null, accessToken: u.accessToken ?? null,
+                         photoURL: u.photoURL ?? null, phoneNumber: u.phoneNumber ?? null, isAdmin: cusDoc?.isAdmin ?? false
+                     }
+                     authStore.setUserToLocal(user);
+                     resolve(user);
+                 } else {
+                     resolve(null);
+                 }
+            }); 
+        });
+    }
+}
+
+export default AuthModel;
--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,25 @@
 import { checkSubscriptionStatus, subscriptionCheck } from "../middleware/subscriptionCheck";
 import { createRouter, createWebHistory } from "vue-router";
-import { onAuthStateChanged } from "firebase/auth";
-import { useAuthStore } from "../stores/auth";
-import { useUserStore } from "../stores/user";
-import { auth } from "../plugins/firebase";
 import routes from "./routes.js";
+import AuthModel from "../models/Auth";
 
+const auth = new AuthModel();
 const router = createRouter({
     history:createWebHistory(),
     routes: routes
 });
 
-let firebaseInit = false;
-
-router.beforeEach((to, from, next) => {
-    if(!firebaseInit) {
-      onAuthStateChanged(auth, (user) => {
-        const authStore = useAuthStore();
-        const userStore = useUserStore();
-        if(user) {
-          const appUser = {
-            uid: user.uid,
-            name: user.displayName,
-            email: user.email,
-          }
-          authStore.setUserToLocal(appUser);
-          userStore.setUser(user);
-          //await userStore.initStar();
-        }
-      });
-      if(!firebaseInit) {
-        firebaseInit = true;
-        next();
-      } else {
-        next('/')
-      }
-    } else {
-      if(to.meta.requiresAuth) {
-        const authStore = useAuthStore();
-        if(authStore?.user || auth?.currentUser) {
-          if(to.meta.requiresAdmin) {
-            if(auth?.currentUser?.isAdmin) {
-              next()
-            } else {
-              next('/')
-            }
-          } else {
-            next();
-          }
-        } else {
-          next('/login');
-        }
-      } else {
-        next();
-      }
-    }
+router.beforeEach(async (to, from, next) => {
+  const user = await auth.fireAuthState();
+  if(to.meta.requiresAuth && to.meta.requiresAdmin) {
+    if(user?.isAdmin) next()
+    else next(false);
+  } else if (to.meta.requiresAuth) {
+    if(user) next();
+    else next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
