5918f940bf0186c9de3472568f651c3e1a91993b
Francis Canayon <canayonfrancis@gmail.com>
2023-08-23 12:53:02 +0800

    Change button content and create swal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
     import { onMounted, ref } from "vue";
     import expressModel from "../models/express";
+    import Swal from 'sweetalert2';
+    import 'sweetalert2/dist/sweetalert2.min.css';
 
     const hovered = ref(false);
     const animationPaused = ref(false);
     const backEndModel = new expressModel();
     const testimonials = ref();
-
+    const email = ref('');
     
 
     onMounted(() => {
@@ -23,6 +25,70 @@
     const mouseenter = () => {
         hovered.value = ref(true)
     }
+
+    const emailLogin = () => {
+        // Swal.fire({
+        //     html: `
+        //         <h2 class="swal2-title text-secondary-color pb-4 pt-2" id="swal2-title" style="display: block;">Email Address</h2>
+        //         <div class="relative">
+        //             <input v-model="email" type="email" id="default-search" class="block w-full py-3 px-4 pr-10 text-sm text-gray-900 font-semibold border border-gray-300 rounded-full bg-white focus:ring-transparent focus:border-main-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-transparent" :placeholder="route.path === '/members' ? 'Explore Search Tools...' : 'Search generated card...'" required>
+        //             <div class="absolute inset-y-0 right-0 flex items-center pr-5 pointer-events-none">
+        //                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+        //                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 2-8.4 7.05a1 1 0 0 1-1.2 0L1 2m18 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1m18 0v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2"/>
+        //                 </svg>
+        //             </div>
+        //         </div>
+        //     `,
+        //     width: 500,
+        //     padding: '',
+        //     color: '#716add',
+        //     allowOutsideClick: true,
+        //     confirmButtonText: 'Submit',
+        //     backdrop: `
+        //         rgba(0, 0, 123, 0.4)
+        //         left top
+        //         no-repeat
+        //     `
+        // }).then((result) => {
+        //     if (result.isConfirmed) {
+        //         console.log(email);
+        //     }
+        // });
+        Swal.fire({
+            title: 'Email Address',
+            input: 'email',
+            inputPlaceholder: 'Example@email.xxx',
+            customClass: {
+                title: 'text-main-color',
+                input: 'text-gray-900 font-semibold border border-gray-300 rounded-full bg-white focus:ring-transparent focus:border-main-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-transparent'
+            }, 
+            showCancelButton: true,
+            confirmButtonText: 'Submit',
+            showLoaderOnConfirm: true,
+            preConfirm: (email) => {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                    if (email === 'example@email.com') {
+                        Swal.fire.showValidationError(
+                        'This email is already taken.'
+                        )
+                    }
+                    resolve()
+                    }, 2000)
+                })
+            },
+            allowOutsideClick: false
+        }).then((result) => {
+            if (result.value) {
+			    Swal.fire({
+			      type: 'success',
+			      title: 'Thankyou',
+			      html: 'Submitted email: ' + result.value
+			    })
+                email.value = result.value
+			}
+        });
+    }
     
 </script>
 
@@ -32,11 +98,14 @@
             <div class="sm:w-[35rem] text-center">
                 <h1 class="font-bold text-2xl sm:text-6xl mb-8 text-white drop-shadow-[10px_10px_2px#000] ">The ultimate companion for every teacher.</h1>
                 <p class=" text-white mb-10 drop-shadow-[0px_2px_10px_#24242a] text-lg bg-[#24242a]">Empowering educators, Fetchy integrates AI. While meetings and lesson planning continue, your methods evolve radically.</p>
-                <router-link :to="{ name: 'pricing' }">
-                    <button type="button" class="transition duration-240 rounded-md bg-main-color hover:shadow-md hover:shadow-[#969cf9] hover:bg-secondary-color border-0 w-fit bg-main-color text-white focus:ring-4 focus:outline-none focus:ring-secondary-color dark:focus:ring-main-color font-medium rounded-lg text-lg px-5 py-2.5 inline-flex justify-center text-center">
-                        Register for FREE!
+                <!-- <router-link :to="{ name: 'pricing' }">
+                    <button type="button" @ class="transition duration-240 rounded-md bg-main-color hover:shadow-md hover:shadow-[#969cf9] hover:bg-secondary-color border-0 w-fit bg-main-color text-white focus:ring-4 focus:outline-none focus:ring-secondary-color dark:focus:ring-main-color font-medium rounded-lg text-lg px-5 py-2.5 inline-flex justify-center text-center">
+                        Request Access
                     </button>
-                </router-link>
+                </router-link> -->
+                <button type="button" @click="emailLogin()" class="transition uppercase duration-240 hover:shadow-md hover:shadow-[#969cf9] hover:bg-secondary-color border-0 w-fit bg-main-color text-white focus:ring-4 focus:outline-none focus:ring-secondary-color dark:focus:ring-main-color font-semibold rounded-lg text-lg px-5 py-2.5 inline-flex justify-center text-center">
+                    Request Access
+                </button>
             </div>
         </div>
         <div class="mt-[100vh] w-[100vw]  absolute left-0 top-0 bg-center bg-black py-10">
