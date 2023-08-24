9a5ed76c5deb0b072b2479f0f9f65a6b388d54af
Alvintooong <aeptechnologies@gmail.com>
2023-08-24 22:14:57 +0800

    Transfer creds to env for production

--- server/plugins/firebase-handler.js
+++ server/plugins/firebase-handler.js
@@ -2,8 +2,20 @@ import { initializeApp, cert } from "firebase-admin/app";
 import 'dotenv/config';
 
 export default initializeApp({
-    credential: cert(process.env.GOOGLE_APPLICATION_CREDENTIALS),
-    projectId: process.env.VITE_FIREBASE_PROJECTID,
+    credential: cert({
+        "type": process.env.FIREBASE_TYPE,
+        "project_id": process.env.FIREBASE_PROJECTID,
+        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+        "client_id": process.env.FIREBASE_CLIENT_ID,
+        "auth_uri": process.env.FIREBASE_AUTH_URI,
+        "token_uri": process.env.FIREBASE_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+        "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL,
+        "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN,
+    }),
+    projectId: process.env.FIREBASE_PROJECTID,
 });
 
 
