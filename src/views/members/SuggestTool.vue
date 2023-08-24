96a2cc5e7aeb209c33418ae73616ac4a37c9dccc
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-08-24 18:03:38 +0800

    Added bongo cat to suggestTool page

--- src/views/members/SuggestTool.vue
+++ src/views/members/SuggestTool.vue
@@ -8,14 +8,19 @@
     const message = ref('')
 </script>
 <template>
-    <div class="w-1/3 p-6 bg-white rounded-lg shadow-md">
-        <Input size="md" label="Name" v-model="name" class="mb-2" />
-        <Input size="md" label="Email" placeholder="name@gmail.com" v-model="email" type="email" class="mb-2" />
-        <Input size="md" label="Tool Name" v-model="tool_name" class="mb-2" />
-        <Input size="md" label="Tool Category" v-model="tool_category" class="mb-2" />
-        <Textarea rows="4" placeholder="Tool Description" v-model="tool_description" label="Tool Description" class="mb-2" />
-        <div class="flex justify-center">
-            <Button type="submit" size="lg" class="w-80 bg-main-color text-center hover:bg-secondary-color mt-4 border-0 text-sm font-semibold">Submit</Button>
+    <div class="flex flex-wrap flex-reverse">
+        <div class="md:w-1/2 lg:w-1/3 p-6 bg-white rounded-lg shadow-md mx-3 ">
+            <Input size="md" label="Name" v-model="name" class="mb-2" />
+            <Input size="md" label="Email" placeholder="name@gmail.com" v-model="email" type="email" class="mb-2" />
+            <Input size="md" label="Tool Name" v-model="tool_name" class="mb-2" />
+            <Input size="md" label="Tool Category" v-model="tool_category" class="mb-2" />
+            <Textarea rows="4" placeholder="Tool Description" v-model="tool_description" label="Tool Description" class="mb-2" />
+            <div class="flex justify-center">
+                <Button type="submit" size="lg" class="w-80 bg-main-color text-center hover:bg-secondary-color mt-4 border-0 text-sm font-semibold">Submit</Button>
+            </div>
+        </div>
+        <div class="md:w-1/2 lg:w-[60%] p-6 bg-white rounded-lg shadow-md mx-3">
+            <img src="/bongo-cat/slap-cat.gif" alt="" class="m-auto w-fit">
         </div>
     </div>
 </template>
