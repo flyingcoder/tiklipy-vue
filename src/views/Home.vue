e84ec7447a82924bc66899015e8b20baa62d9f5b
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-08-25 16:54:36 +0800

    slowed the testimonial in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -91,7 +91,7 @@
             </button>
         </div>
         <div :style="'width: '+ 26 * testimonials?.length +'rem'" class="sm:mt-[92vh] mt-[85vh] absolute left-0 top-0 bg-center bg-gradient-to-t from-[#f3f4f6] from-60% py-10 flex overflow-x-hidden justify-center" @mouseenter="toggleAnimation" @mouseleave="toggleAnimation">
-            <div :style="'animation-duration:'+5 * testimonials?.length +'s'" :class="[hovered ? '' : 'animate-r_marquee2', { 'animation-paused': animationPaused }]" class="flex justify-around w-full max-w-none">
+            <div :style="'animation-duration:'+15 * testimonials?.length +'s'" :class="[hovered ? '' : 'animate-r_marquee2', { 'animation-paused': animationPaused }]" class="flex justify-around w-full max-w-none">
                 <div v-for="(testimonial, index) in testimonials" :key="index + '-testi-cards'" class="w-[26rem] mr-10 text-4xl  bg-white p-5 rounded-2xl flex flex-wrap content-between">
                     <div class="">
                         <div class="flex flex-wrap mb-5 text-lg stars">
@@ -108,7 +108,7 @@
                     </div>
                 </div>
             </div>
-            <div :style="'animation-duration:'+5 * testimonials?.length +'s'" :class="[hovered ? '' : 'animate-r_marquee', { 'animation-paused': animationPaused }]" class="absolute flex justify-around w-full max-w-none">
+            <div :style="'animation-duration:'+15  * testimonials?.length +'s'" :class="[hovered ? '' : 'animate-r_marquee', { 'animation-paused': animationPaused }]" class="absolute flex justify-around w-full max-w-none">
                 <div v-for="(testimonial, index) in testimonials" :key="index + '-testi-cards'" class="w-[26rem] mr-10 text-4xl  bg-white p-5 rounded-2xl flex flex-wrap content-between ">
                     <div class="">
                         <div class="flex flex-wrap mb-5 text-lg stars">
