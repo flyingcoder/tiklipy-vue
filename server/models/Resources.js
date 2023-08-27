372fe38ab5ba288cd51f883a80e000d08c750506
Francis Canayon <canayonfrancis@gmail.com>
2023-08-27 23:30:33 +0800

    Transfer function to server

--- server/models/Resources.js
+++ server/models/Resources.js
@@ -1,10 +1,12 @@
 import admin from "../plugins/firebase-handler.js";
 import { getFirestore } from "firebase-admin/firestore";
+import { useAuthStore } from "../../src/stores/auth";
 
 class ResourcesModel {
     constructor() {
         const db = getFirestore(admin);
         this.col = db.collection('generatedResources');
+        this.authStore = new useAuthStore();
     }
 
     async getResources(uid) {
@@ -14,6 +16,22 @@ class ResourcesModel {
             if(!snaps.empty) return true;
         } catch (error) {
             console.error('Error in getting all resources', error);
+            return false;
+        }
+    }
+
+    async getResource(docId) {
+        try {
+            const docRef = this.col.doc(docId);
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) return docSnap.data();
+            return false;
+            // const docRef = doc(db, this.collectionName, docId);
+            // const snap = await getDoc(docRef);
+            // if(snap.exists()) return snap.data();
+            // else return false;
+        } catch (error) {
+            console.error('Error in getting all resource', error);
             return false;
         }
     }
