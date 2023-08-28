340792faa17b7f8e1fc13c8451884b212814aaf5
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-08-29 07:21:18 +0800

    Added color to features

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -30,7 +30,7 @@
         <div v-for="(feature, index) in features" :key="index + '-features-cards'" :class="'hover:bg-'+getColorClass(index)+'-100 hover:shadow-'+getColorClass(index)+'-200'" class="border-[1px] border-color-main-color w-full sm:w-[49%] midlg:w-[32%] mb-5 bg-white  p-5 2xl:p-10  content-between rounded-xl hover:scale-105 ease-linear duration-200 shadow-sm">
             <div class="overflow-hidden  w-full mb-[4rem]">
                 <div class="rounded-[8px]  bg-main-color text-4xl py-1 px-2 w-fit">
-                    <i class="ti" :class="feature.icon" alt=""></i>
+                    <i class="ti text-white" :class="feature.icon" alt=""></i>
                 </div>
             </div>
             <div class="">
