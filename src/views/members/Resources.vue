639c21f686d133f1215cea551d92552634c54091
Alvintooong <aeptechnologies@gmail.com>
2023-09-02 09:53:08 +0800

    Fix generated resource display issue

--- src/views/members/Resources.vue
+++ src/views/members/Resources.vue
@@ -1,35 +1,45 @@
 <script setup>
     import { onMounted, ref, computed } from 'vue';
+    import { auth } from "../../plugins/firebase";
     import GeneratedResourceModel from "../../models/generatedResources";
     import { useAuthStore } from '../../stores/auth';
     import { useFormStore } from '../../stores/form';
+    import { useLoaderStore } from '../../stores/loader';
     import HeaderFilter from '../../components/HeaderFilter.vue';
     import { useRouter, useRoute } from 'vue-router';
+    import { onAuthStateChanged } from 'firebase/auth';
 
     const router = useRouter();
     const route = useRoute();
     const formStore = useFormStore();
     const authStore = useAuthStore();
-    const resources = ref([]);
-    let resourcesRaw = [];
+    const loaderStore = useLoaderStore();
+    const resourcesRaw = ref();
     const resourceModel = new GeneratedResourceModel();
-
-
-
-    onMounted(async () => {
-        resourcesRaw = await resourceModel.getGeneratedResources();
-        resourcesFunc()
-    });
     
-    const resourcesFunc = () => {
-        if(route.params.type !== "all") {
-            resources.value = resourcesRaw.filter(item => item.data.type.toLowerCase().includes(route.params.type.toLowerCase()));
-        } else {
-            resources.value = resourcesRaw
+    onAuthStateChanged(auth, async (user) => {
+        if(user?.accessToken) {
+            await getResources();
+            loaderStore.isLoading = false;
         }
+    });
+
+    onMounted(() => {
+        if(resourcesRaw.value == undefined)
+            loaderStore.isLoading = true;
+    });
+
+    const getResources = async () => {
+        resourcesRaw.value = await resourceModel.getGeneratedResources().then((res) => res);
+        console.log(resourcesRaw.value);
     }
 
-
+    const resources = computed(() => {
+        if(route.params.type === "all")
+            return resourcesRaw.value?.data ?? [];
+        else
+            return resourcesRaw.value?.data?.filter(item => item.type.toLowerCase().includes(route.params.type.toLowerCase())) ?? [];
+    });
 </script>
 <template>
     <div class="flex flex-wrap justify-between px-3 mt-7">
@@ -38,20 +48,20 @@
                 <div class="w-full">
                     <HeaderFilter/>
                     <div class="flex -mx-2">
-                        <div v-for="resource in resources" :key="resource.data.teacherId + '-resource-card'" class="cursor-pointer w-full sm:1/2 md:w-1/3 lg:w-1/5 m-2 bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain hover:shadow-lg">
+                        <div v-for="(resource, index) in resources" :key="index*2+1 + '-resource-card-'" class="cursor-pointer w-full sm:1/2 md:w-1/3 lg:w-1/5 m-2 bg-white rounded-lg sm:mr-5 mb-5 bg-[url('/p-1.png')] bg-no-repeat bg-contain hover:shadow-lg">
                             <router-link :to="{ name: 'resource', params:{ id: resource.id } }" class="">
                                 <div class="p-4 max-xs:!p-2 ">
                                     <div class="flex justify-between mb-3">
                                         <div class="flex items-center justify-start">
-                                            <i :class="resource.data.icon" class="text-xl ti"></i>
+                                            <i :class="resource.icon" class="text-xl ti"></i>
                                         </div>
                                         <div class="px-4 text-white uppercase bg-green-500 border rounded-md text-md">
-                                            {{ resource.data.type }}
+                                            {{ resource.type }}
                                         </div>
                                     </div>
                                     <div class="flex flex-col justify-between leading-normal">
                                         <h5 class="text-lg font-bold tracking-tight text-right line-clamp-2 wh lg:text-xl text-main-color dark:text-white">
-                                            {{ resource.data.title }}
+                                            {{ resource.title }}
                                         </h5>
                                     </div>
                                 </div>
