750520b3341ae7978cf0f60fb43ee047262100d3
Francis Canayon <canayonfrancis@gmail.com>
2023-09-02 22:27:14 +0800

    Add quill editor in slides page

--- src/views/members/Slide.vue
+++ src/views/members/Slide.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
+    import { QuillEditor } from '@vueup/vue-quill';
 
     const activeDiv = ref(1);
     const content = ref('');
@@ -8,43 +9,54 @@
         activeDiv.value = divNumber;
     };
 
-    const editableText1 = ref('Initial text for paragraph 1');
-    const editableText2 = ref('Initial text for paragraph 2');
-    // Add more editableText properties for other divs as needed
+    const activeCard = computed(() => {
+        return cards.value.find((card) => card.id === activeDiv.value);
+    });
 
-    // Method to handle dropping content into the drop area
-    const handleDrop = (event) => {
-        event.preventDefault();
-        const draggedElementId = event.dataTransfer.getData('text/plain');
-        const targetElement = document.getElementById(draggedElementId);
+    const toolbarOptions = [
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+        [{ 'header': 1 }, { 'header': 2 }],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],   
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['clean']
+    ];
 
-        if (targetElement) {
-            // Get the active div number
-            const activeDivNumber = activeDiv.value;
+    const cards = ref([
+        {
+            id: 1,
+            text: '',
+        },
+        {
+            id: 2,
+            text: '',
+        },
+        {
+            id: 3,
+            text: '',
+        },
+        {
+            id: 4,
+            text: '',
+        },
+    ]);
 
-            // Update the content based on the active div
-            if (activeDivNumber === 1) {
-                content.value = editableText1.value;
-            } else if (activeDivNumber === 2) {
-                content.value = editableText2.value;
-            }
-            // Add similar conditions for other divs as needed
-        }
-    };
 </script>
+
 <template>
     <div class="flex flex-wrap bg-white mt-7 rounded-xl max-lg:flex-col-reverse">
         <div class="block justify-between w-full lg:w-1/5 xs:py-5">
-            <div class="flex py-2 pr-2" @click="setActiveDiv(1)" :class="{ 'bg-indigo-100': activeDiv === 1 }">
-                <h2 class="w-6 h-6 ml-2 text-black">1</h2>
+            <div v-for="card in cards" :key="card.id" class="flex py-2 pr-2" @click="setActiveDiv(card.id)" :class="{ 'bg-indigo-100': activeDiv === card.id }">
+                <h2 class="w-6 h-6 ml-2 text-black">{{ card.id }}</h2>
                 <div class="flex bg-gray-300 h-40 md:w-[40%] lg:w-full object-cover object-center rounded-lg">
-                    <p>{{ editableText1 }}</p>
-                </div>
-            </div>
-            <div class="flex py-2 pr-2" @click="setActiveDiv(2)" :class="{ 'bg-indigo-100': activeDiv === 2 }">
-                <h2 class="w-6 h-6 ml-2 text-black">2</h2>
-                <div class="flex bg-gray-300 h-40 md:w-[40%] lg:w-full object-cover object-center rounded-lg">
-                    <p>{{ editableText2 }}</p>
+                    <p>{{ card.text }}</p>
                 </div>
             </div>
         </div>
@@ -53,7 +65,7 @@
                 class="h-[40rem] bg-gray-300 w-full block object-cover object-center rounded-lg p-4 xs:px-16"
                 id="contentDisplay"
             >
-                <div>{{ content }}</div>
+                <QuillEditor v-if="activeCard" theme="snow" :toolbar="toolbarOptions" v-model="activeCard.text" contentType="html" class="text-black !border-none" />
             </div>
         </div>
     </div>
