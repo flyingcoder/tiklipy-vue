ee38987bb55bcd6fed4c7539a34f0f3f5ebb7f7d
Francis Canayon <canayonfrancis@gmail.com>
2023-09-04 22:51:51 +0800

    Add prompt new functionalities

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import App from './App.vue'
 import { pinia }  from './stores'
 import router from './router'
 import { createApp } from 'vue'
+import filters from './filters.js'
 
 const app = createApp(App);
 
+app.config.globalProperties.$filters = filters;
+
 app.use(pinia);
+
 app.use(router);
 
 app.mount('#app');
--- src/views/members/Dashboard.vue
+++ src/views/members/Dashboard.vue
@@ -16,16 +16,7 @@
     const backEndModel = new expressModel();
     const searchQuery = ref('');
     const rawCards = ref();
-    const tags = ref([
-        { name: 'featured', desc: ''},
-        { name: 'lessons', desc: 'Effortlessly generate a lesson plan tailored to your teaching style.'},
-        { name: 'tools', desc: 'Empowering Efficiency: Unleashing the Potential of Essential Tools.'},
-        { name: 'explore', desc: 'Unveiling the Unknown: Embarking on Journeys of Exploration'},
-        { name: 'manage', desc: 'Navigating Success: Mastering the Art of Effective Management'},
-        { name: 'organize', desc: 'Harmonize and Systematize: The Essence of Effective Organization'},
-        { name: 'modify', desc: 'Crafting Change: The Art of Skillful Modification'},
-        { name: 'write', desc: 'Effortlessly Generate Diverse Content with Personalized Style and Substance.'},
-    ]);
+    const tags = formStore.tags;
     
     onAuthStateChanged(auth, async (user) => {
         if(user?.accessToken) {
@@ -72,11 +63,11 @@
                                 {{ card.category }}
                             </div>
                             <div class="flex flex-col justify-between leading-normal">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight dark:text-white">
+                                <h5 class="capitalize mb-2 text-2xl font-bold tracking-tight dark:text-white">
                                     {{ card.title }}
                                 </h5>
                                 <p class="font-normal text-gray-500 dark:text-gray-400">
-                                    {{ card.description }}
+                                    {{ $filters.capitalizeSentencesInParagraph(card.description) }}
                                 </p>
                             </div>
                         </div>
