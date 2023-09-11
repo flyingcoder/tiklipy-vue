37e829906bf5ee791e6233b40be01e24e0087a7b
Francis Lawrenz B. Canayon <64053364+canayonfrancis@users.noreply.github.com>
2023-09-11 12:26:54 +0800

    Review/function (#382)
    * Add condition for review button if the inputs have value
    
    * Add functionalities when submitting review and add additional design

--- src/views/members/Review.vue
+++ src/views/members/Review.vue
@@ -1,6 +1,6 @@
 <script setup>
-    import { ref } from 'vue';
-    import { Textarea, Button, Input } from 'flowbite-vue';
+    import { ref, computed } from 'vue';
+    import { Textarea, Button, Input, Alert } from 'flowbite-vue';
     import { useAuthStore } from '../../stores/auth';
     import expressModel from "../../models/express";
     import dayjs from "dayjs";
@@ -10,6 +10,7 @@
     const name = ref('');
     const role = ref('');
     const selectedRating = ref(0);
+    const showReviewAlert = ref(false);
     const star = ref(0);
     const hoverRating = ref(0);
     const ratings = [1, 2, 3, 4, 5];
@@ -27,6 +28,14 @@
     function clearHover() {
         hoverRating.value = 0;
     }
+    
+    const haveContent = computed(() => {
+        return selectedRating.value > 0 &&
+            name.value.trim() !== '' &&
+            role.value.trim() !== '' &&
+            message.value.trim() !== '';
+    });
+
 
     const submitReview = () => {
         const review = {
@@ -40,9 +49,24 @@
         }
 
         backEndModel.addReviews(review);
+
+        name.value = '';
+        role.value = '';
+        star.value = 0;
+        selectedRating.value = 0;
+        message.value = '';
+
+        showReviewAlert.value = true;
+
+        setTimeout(() => {
+            showReviewAlert.value = false;
+        }, 8000);
     }
 </script>
 <template>
+    <Alert type="success" v-if="showReviewAlert" closable title="Thankyou for Your Feedback" :inline="false" :class="showReviewAlert ? 'animate__animated animate__fadeInRightBig' : 'animate__animated animate__fadeOutRightBig'" class="mb-2 fixed bottom-0 right-0 w-[400px]">
+        Thank you so much for taking the time to leave a review! Your feedback is greatly appreciated, and it means a lot to us.
+    </Alert>
     <div class="flex flex-wrap max-md:flex-col-reverse">
         <div class="md:w-[48%] lg:w-1/3 p-6 bg-white text-gray-800 rounded-lg shadow-md m-1 lg:mx-3 ">
             <div class="text-black mb-2">
@@ -54,11 +78,11 @@
                     </label>
                 </div>
             </div>
-            <Input size="md" label="Name" v-model="name" class="mb-2" />
-            <Input size="md" label="Role" v-model="role" class="mb-2" />
-            <Textarea rows="4" placeholder="Review" v-model="message" label="Your Review" class="mb-4" />
+            <Input size="md" label="Name" v-model="name" class="mb-2" required/>
+            <Input size="md" label="Role" v-model="role" class="mb-2" required />
+            <Textarea rows="4" placeholder="Review" v-model="message" required label="Your Review" class="mb-4" />
             <div class="flex justify-center">
-                <Button type="submit" size="lg" @click="submitReview" class="w-full bg-main-color text-center hover:bg-secondary-color mt-4 border-0 text-sm font-semibold">Submit</Button>
+                <Button type="submit" size="lg" @click="submitReview" :disabled="!haveContent" impo class="w-full bg-main-color text-center hover:bg-secondary-color mt-4 border-0 text-sm font-semibold">Submit</Button>
             </div>
         </div>
         <div class="md:w-[48%] lg:w-[60%] p-6 bg-[#fbfbfb] rounded-lg shadow-md m-1 lg:mx-3 ">
