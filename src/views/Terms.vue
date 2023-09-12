5d0c1553fbe0b695f81a85914c5f692abe6a254e
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-09-12 21:02:01 +0800

    added padding on terms pages

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <div class="container mx-auto p-3 md:py-24 md:my-10 text-black">
-        <div class="max-w-3xl mx-auto p-24 bg-white rounded-xl shadow-md">
+    <div class="container mx-auto p-3 md:py-8 text-black">
+        <div class="max-w-3xl mx-auto p-4 sm:p-10 md:p-24 bg-white rounded-xl shadow-md">
             <h1 class="text-4xl font-bold text-gray-700 mb-4 text-center">Terms of Use - Tiklipy</h1>
             <p class="text-gray-700 font-semibold text-lg text-center">August 29, 2023</p>
 
