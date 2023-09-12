04aba6d331af36c2a7e7949ccab0e4a2e06b12fc
flyingcoder-2 <reyanshcoder1234@gmail.com>
2023-09-12 20:53:48 +0800

    revert about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
 </script>
 <template>
     <div class="block px-2 py-8 my-10 text-black">
-        <div class="flex transition ease-in-out bg-contain bg-center bg-fixed bg-black delay-150 h-[800px] min-h-[800px] sm:w-[100vw] bg-[center_-5rem] items-start justify-center absolute left-0 right-0 top-0 bg-[url(/About-background.png)] bg-no-repeat bg-white">
-            <div class="sm:w-[40rem] w-[35rem] text-center  pt-[180px]">
+        <div class="pt-1 pb-40 mx-auto max-w-screen-midlg">
+            <div class="block">
                 <div class="mb-8 text-4xl font-bold text-center text-gray-800 lg:text-7xl">
                     Forging an innovative route to reshape<br> the landscape of education.
                 </div>
@@ -13,43 +13,43 @@
                 </div>
             </div>
         </div>
-        <div class="container mx-auto sm:mt-[700px] mt-[800px] left-0 top-0 bg-center bg-gradient-to-t from-[#f3f4f6] from-60% py-10 flex overflow-x-hidden justify-center">
-            <div class="flex flex-wrap pt-16 bg-white rounded-b-2xl">
+        <div class="container mx-auto ">
+            <div class="max-w-screen-sm m-auto mb-5 text-3xl font-semibold text-center text-gray-700">
+                    An organization characterized by empathy and dedication.
+            </div>
+            <div class="max-w-screen-sm pb-10 m-auto text-lg text-center text-gray-800 font-base">
+                    We formulated Tiklipy with a unique approach, distinguishing it from other educational companies.
+                </div>
+            <div class="flex flex-wrap justify-around mt-7">
+                <div class="block mt-5 w-96">
+                    <img src="/Boss.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
+                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">Crafted by a Teaching Professional</h2>
+                    <p class="text-lg text-center text-gray-500">Tiklipy was meticulously designed by an experienced educator who deeply understands the needs of fellow teachers.</p>
+                </div>
+                <div class="block mt-5 w-96">
+                    <img src="/Graduation-cap.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
+                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">AI possessing an educational degree</h2>
+                    <p class="text-lg text-center text-gray-500">Our AI is far from ordinary; it's uniquely educated in every facet of teaching.</p>
+                </div>
+                <div class="block mt-5 w-96">
+                    <img src="/Trophy.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
+                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">Honoring our champions</h2>
+                    <p class="text-lg text-center text-gray-500">Our goal is to serve as a steadfast teaching companion, prioritizing the requirements of educators above everything else.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mx-auto">
+            <div class="flex flex-wrap pt-16 mt-7">
                 <div class="block lg:w-1/2 lg:px-10 place-self-center">
                     <img src="/creator.png" class="mx-auto mb-4" alt="Gift" >
                 </div>
                 <div class="block py-4 lg:w-1/2 place-self-center">
                     <h2 class="mb-4 text-4xl font-semibold text-gray-800 max-sm:text-center">Words from the creators</h2>
                     <p class="mt-10 text-lg text-gray-500 max-sm:text-center">
-                        Greetings, we're the group responsible for Tiklipy.<br><br>
-                        Embarking on this path more than three years ago, we were propelled by a vision to empower educators and transform the landscape of education. Our intuitive tools simplify tasks for teachers worldwide, and we're only at the beginning of our journey.<br><br>
+                        Greetings, we're the groups responsible for Tiklipy.<br><br>
+                        Embarking on this path more than three years ago, we were propelled by a vision to empower educators and transform the landscape of education. Our intuitive tools simplify tasks for teachers worldwide, and we're only at the beginning of our journey.<br><br><br>
                         Experience it firsthand. We're eagerly anticipating your feedback!
                     </p>
-                </div>
-            </div>
-        </div>
-        <div class="container mx-auto bg-white ">
-            <div class="max-w-screen-sm m-auto mb-5 text-3xl font-semibold text-center text-gray-700">
-                    An organization characterized by empathy and dedication.
-            </div>
-            <div class="max-w-screen-sm pb-10 m-auto text-lg text-center text-gray-800 font-base">
-                We formulated Tiklipy with a unique approach, distinguishing it from other educational companies.
-            </div>
-            <div class="flex flex-wrap justify-around mb-0">
-                <div class="block mt-5 w-96 bg-white p-10 pt-0 rounded-2xl">
-                    <img src="/Boss.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
-                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">Crafted by a Teaching Professional</h2>
-                    <p class="text-lg text-center text-gray-500">Tiklipy was meticulously designed by an experienced educator who deeply understands the needs of fellow teachers.</p>
-                </div>
-                <div class="block mt-5 w-96 bg-white p-10 pt-0 rounded-2xl">
-                    <img src="/Graduation-cap.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
-                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">AI possessing an educational degree</h2>
-                    <p class="text-lg text-center text-gray-500">Our AI is far from ordinary; it's uniquely educated in every facet of teaching.</p>
-                </div>
-                <div class="block mt-5 w-96 bg-white p-10 pt-0 rounded-2xl">
-                    <img src="/Trophy.svg" class="mx-auto mb-4" alt="Gift" width="150" height="150">
-                    <h2 class="mb-4 text-2xl font-semibold text-center text-gray-800">Honoring our champions</h2>
-                    <p class="text-lg text-center text-gray-500">Our goal is to serve as a steadfast teaching companion, prioritizing the requirements of educators above everything else.</p>
                 </div>
             </div>
         </div>
